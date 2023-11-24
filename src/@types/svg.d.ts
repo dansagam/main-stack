ca8017b93c3771274150e58e058122075ce80a53
Kayode Ogunnowo <48393847+dansagam@users.noreply.github.com>
2023-11-24 21:05:45 +0100

    import: the svg for the sidebar

--- src/@types/svg.d.ts
+++ src/@types/svg.d.ts
@@ -1,6 +1,26 @@
 // https://github.com/facebook/create-react-app/blob/0ee4765c39f820e5f4820abf4bf2e47b3324da7f/packages/react-scripts/lib/react-app.d.ts#L47-L56
 
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module '*.svg?react' {
+  import * as React from 'react';
+
+  const ReactComponent: React.FunctionComponent<React.ComponentProps<'svg'> & { title?: string }>;
+
+  export default ReactComponent;
+}
+
 declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg?import' {
   import * as React from 'react';
 
   export const ReactComponent: React.FunctionComponent<
